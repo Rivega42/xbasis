da82d93a0fd970b32fbd80b9596ca68a27d5086b
Роман Гудков <39859935+Rivega42@users.noreply.github.com>
2026-01-25 21:07:21 +0300

    feat(web): add UI components (badge, dropdown-menu, table, tabs)

---
+++ web/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from './button';
+export * from './card';
+export * from './input';
+export * from './badge';
+export * from './tabs';
+export * from './table';
+export * from './dropdown-menu';
